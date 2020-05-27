b47d0afcccbd77b3c8bb15f2a8a44ee5757325fb
Isabela Porto <isamporto@hotmail.com>
2020-05-27 16:13:08 -0300

    Create routes and controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,21 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants...'
+spoleto = Restaurant.new(name: "spoleto", category: "italian", address: "Shopping Morumbi")
+spoleto.save!
+puts 'Save spoleto'
+liglig = Restaurant.new(name: "Lig-lig", category: "chinese", address: "Rua Ântonio Macedo")
+liglig.save!
+puts "Save Liglig"
+gusteau = Restaurant.new(name: "Gusteau's", category: "french", address: "Paris")
+gusteau.save!
+puts "Save Gusteau's"
+belgian = Restaurant.new(name: "Wafflery", category: "belgian", address: "Avenida Paulista")
+belgian.save!
+puts "Save Waffle"
+ratatouile = Restaurant.new(name: "Ratatouille", category: "french", address: "Disneyland")
+ratatouile.save!
+puts 'Finished!'
